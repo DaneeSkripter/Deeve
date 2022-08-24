9550ca48fd9063274b310a9c40d263231191c864
DaneeSkripter <63862773+DaneeSkripter@users.noreply.github.com>
2022-08-24 10:56:14 +0200

    ping command

--- commands/ping.js
+++ commands/ping.js
@@ -1,13 +1,15 @@
 const { Command, CommandType } = require('gcommands');
-
+const Discord = require('discord.js')
 // Create a new command with the name 'hello'
 new Command({
 	name: 'ping',
-	description: 'Says pong!',
+	description: 'Shows bot ping',
 	// GCommands Next offers different types of commands, we will only use slash and message commands here.
 	type: [CommandType.SLASH, CommandType.MESSAGE],
 	// The function thats executed when the user uses the command.
 	run: (ctx) => {
-		return ctx.reply(`pong!`);
+		const embed = new Discord.MessageEmbed
+		embed.setTitle(`🏓 My ping is ${ctx.client.ws.ping} ms`)
+		ctx.reply({ embeds: [embed]})
 	}
 });
