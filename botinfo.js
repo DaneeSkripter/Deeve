ee75ad98f16bb6da55467c0b94d5fae62e780b79
DaneeSkripter <63862773+DaneeSkripter@users.noreply.github.com>
2022-08-24 11:26:18 +0200

    ping set to ephemeral, botinfo command

---
+++ botinfo.js
@@ -0,0 +1,24 @@
+const { Command, CommandType } = require('gcommands');
+const Discord = require('discord.js')
+const ms = require('ms')
+const package = require('../package.json')
+// Create a new command with the name 'hello'
+new Command({
+	name: 'about',
+	description: 'Shows information about bot',
+	// GCommands Next offers different types of commands, we will only use slash and message commands here.
+	type: [CommandType.SLASH, CommandType.MESSAGE],
+	// The function thats executed when the user uses the command.
+	run: (ctx) => {
+		const embed = new Discord.MessageEmbed
+		embed.addFields(
+            { name: 'Uptime', value: ms(ctx.client.uptime, { long: true}), inline: true},
+            { name: 'Discord.js', value: package.dependencies['discord.js'], inline: true},
+            { name: 'Node.js', value: package.nodejsver, inline: true},
+            { name: 'Version', value: 'v1.0.0-beta.1', inline: true},
+            { name: 'Developer', value: 'DaneeSkripter', inline: true},
+            )
+        embed.setColor("WHITE")
+		ctx.reply({ embeds: [embed], ephemeral: true})
+	}
+});
---
+++ ping.js
@@ -0,0 +1,16 @@
+const { Command, CommandType } = require('gcommands');
+const Discord = require('discord.js')
+// Create a new command with the name 'hello'
+new Command({
+	name: 'ping',
+	description: 'Shows bot ping',
+	// GCommands Next offers different types of commands, we will only use slash and message commands here.
+	type: [CommandType.SLASH, CommandType.MESSAGE],
+	// The function thats executed when the user uses the command.
+	run: (ctx) => {
+		const embed = new Discord.MessageEmbed
+		embed.setTitle(`🏓 My ping is ${ctx.client.ws.ping} ms`)
+		embed.setColor("WHITE")
+		ctx.reply({ embeds: [embed], ephemeral: true})
+	}
+});
